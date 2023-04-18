9f2b82cad23e65fc10611a114267c3d07b6368f9
GuilhermeEnrique <guilherme.dantas416@gmail.com>
2023-04-18 10:45:55 -0400

    Tela de Login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
+import Head from "next/head";
 import React from "react";
+import Image from "next/image";
+import styles from '../../styles/home.module.scss';
+
+import logoImg from '../../public/Icon-Dice-Roll.svg';
+
+import { Input } from "../components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Index
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>Dice-Roll - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image className="img" src={logoImg} alt="Logo Dice Roll" />
+
+        <div className={styles.login}>
+          <form>
+            <Input
+            placeholder="Digite seu email"
+            type="text"
+            />
+          
+            <Input
+            placeholder="Digite sua senha"
+            type="password"
+            />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
